a3eda2b46be91b2f4f3c92d34fc2ee685da30b80
Raul C. Pirvan <RaulC.Pirvan@outlook.com>
2024-07-13 22:47:35 +0300

    add banner to homepage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MealCardComponent } from './meal-card/meal-card.component';
 import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
     MealCardComponent,
     AdminNavbarComponent,
+    HomepageComponent,
     
    ],
   imports: [
