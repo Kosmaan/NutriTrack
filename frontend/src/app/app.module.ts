681b32d7d1a1349421ee18936f0914d46cdc60b9
Kosmaan <radu.cosmin.30.1@gmail.com>
2024-07-14 13:36:26 +0300

    added filters component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MealCardComponent } from './meal-card/meal-card.component';
 import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
     MealCardComponent,
     AdminNavbarComponent,
+    FiltersComponent,
     
    ],
   imports: [
