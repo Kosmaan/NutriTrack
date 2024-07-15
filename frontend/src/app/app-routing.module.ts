3667e762ca5727eee83e3983b554e162203ee6e6
iustin948 <nicolaie-iustin.homoranu@student.tuiasi.ro>
2024-07-15 13:17:35 +0300

    added homepage routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path:"home", component:HomepageComponent },
+ 
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
