c904ea2dc721b1599c3a3359a99fb85f1e06873a
Raul C. Pirvan <RaulC.Pirvan@outlook.com>
2024-07-24 12:50:03 +0300

    clean stuff

--- frontend/src/app/pages/admin/components/add-meal/add-meal.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-add-meal',
-  templateUrl: './add-meal.component.html',
-  styleUrls: ['./add-meal.component.scss']
-})
-export class AddMealComponent { }
