5f2f0dbb7a21b8ef362d8b26b5bde0d251f9fdd3
Raul C. Pirvan <RaulC.Pirvan@outlook.com>
2024-07-25 08:26:09 +0300

    create service for contact form

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +8,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private contactService: ContactService) {
+    this.contactForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      this.contactService.sendContactForm(formData).subscribe(
+        (response) => {
+          console.log('Form submission successful', response);
+        },
+        (error) => {
+          console.log('Form submission error', error);
+        }
+      );
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
