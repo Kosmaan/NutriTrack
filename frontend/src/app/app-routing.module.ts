1dc40b3b9efb7604da94c46b24331988a811d729
Lavinia <lavinia-andreea.rachieru@student.tuiasi.ro>
2024-07-25 18:10:01 -0700

    done

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginComponent } from './pages/authentication/login/login.component';
 import { RegisterComponent } from './pages/authentication/register/register.component';
 import { AddMealComponent } from './components/add-meal/add-meal.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
-
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'add-meal', component: AddMealComponent },
-
+  { path: 'aboutus', component: AboutUsComponent },
 ];
 
 @NgModule({
