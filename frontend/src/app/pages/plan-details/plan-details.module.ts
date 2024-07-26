66d27f41fde1be18ff7f7aa54d21fdc24b41e80a
Lavinia <lavinia-andreea.rachieru@student.tuiasi.ro>
2024-07-26 16:28:57 -0700

    plan-details-page

--- frontend/src/app/pages/plan-details/plan-details.module.ts
+++ frontend/src/app/pages/plan-details/plan-details.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DropDownButtonComponent } from './components/drop-down-button/drop-down-button.component';
 
-const COMPONENTS: any[] = [
-  DropDownButtonComponent,
-];
 @NgModule({
-  declarations: [COMPONENTS],
-  exports: [COMPONENTS],
-}}
+  declarations: [],
+  imports: [CommonModule],
+})
+export class PlanDetailsModule {}
