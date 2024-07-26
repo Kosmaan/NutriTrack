0c2fbce4dcf2fd59e40fa6a217c04b964197c871
MihaelaBurla <mihaela-ecaterina.burla@student.tuiasi.ro>
2024-07-26 21:13:36 +0300

    statistics page

--- frontend/src/app/pages/statistics/components/calendar/calendar.component.ts
+++ frontend/src/app/pages/statistics/components/calendar/calendar.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
+    daysOfWeek: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    currentDayIndex: number = 0;
+    displayDays: string[] = [];
+  
+    meals: { [key: string]: string[] } = {
+      'Monday': ['Breakfast', 'Lunch', 'Dinner'],
+      'Tuesday': ['Breakfast', 'Lunch', 'Dinner'],
+      'Wednesday': ['Breakfast', 'Lunch', 'Dinner'],
+      'Thursday': ['Breakfast', 'Lunch', 'Dinner'],
+      'Friday': ['Breakfast', 'Lunch', 'Dinner'],
+      'Saturday': ['Breakfast', 'Lunch', 'Dinner'],
+      'Sunday': ['Breakfast', 'Lunch', 'Dinner']
+    };
+  
+    ngOnInit() {
+      this.updateDisplayDays();
+    }
+  
+    updateDisplayDays() {
+      this.displayDays = [
+        this.daysOfWeek[(this.currentDayIndex - 1 + this.daysOfWeek.length) % this.daysOfWeek.length],
+        this.daysOfWeek[this.currentDayIndex],
+        this.daysOfWeek[(this.currentDayIndex + 1) % this.daysOfWeek.length]
+      ];
+    }
+  
+    next(): void {
+      this.currentDayIndex = (this.currentDayIndex + 1) % this.daysOfWeek.length;
+      this.updateDisplayDays();
+    }
+  
+    previous(): void {
+      this.currentDayIndex = (this.currentDayIndex - 1 + this.daysOfWeek.length) % this.daysOfWeek.length;
+      this.updateDisplayDays();
+    }
 
 }
