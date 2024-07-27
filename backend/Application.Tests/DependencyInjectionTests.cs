006127c82ba90a04ccf7e15f4af2d0f78478bc83
Kosmaan <radu.cosmin.30.1@gmail.com>
2024-07-27 15:44:56 +0300

    added one file service test

---
+++ backend/Application.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using FluentAssertions.Common;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+
+namespace Application.Tests
+{
+    public class DependencyInjectionTests
+    {
+
+        [Fact]
+        public void AddApplicationServices_Should_RegisterAllServices()
+        {
+            var services = new ServiceCollection();
+
+            var _passwordHasher = Substitute.For<IPasswordHasher>();
+            var _authenticationRepository = Substitute.For<IAuthenticationRepository>();
+            var _identityHandler = Substitute.For<IIdentityHandler>();
+
+            services.AddSingleton(_passwordHasher);
+            services.AddSingleton(_authenticationRepository);
+            services.AddSingleton(_identityHandler);
+
+            
+            services.AddApplicationServices();
+            var serviceProvider = services.BuildServiceProvider();
+
+            
+            var authorizationService = serviceProvider.GetService<AuthorizationService>();
+            Assert.NotNull(authorizationService);
+        }
+    }
+}
