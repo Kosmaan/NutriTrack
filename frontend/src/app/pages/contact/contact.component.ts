25ff480110a164f44e790a6857cd04df0fe9c80f
Raul C. Pirvan <RaulC.Pirvan@outlook.com>
2024-07-31 12:05:39 +0300

    add validators for forms

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ContactService } from 'src/app/services/contact.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-contact',
@@ -10,18 +11,19 @@ import { ContactService } from 'src/app/services/contact.service';
 export class ContactComponent {
   contactForm: FormGroup;
 
-  constructor(private contactService: ContactService) {
+  constructor(private contactService: ContactService, private toastService: ToastService) {
     this.contactForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      phone: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
       description: new FormControl('', [Validators.required]),
     });
   }
 
   onSubmit(): void {
     if (this.contactForm.valid) {
+      this.toastService.show('Form submitted successfully!', 'success');
       const formData = this.contactForm.value;
       this.contactService.sendContactForm(formData).subscribe(
         (response) => {
@@ -32,7 +34,11 @@ export class ContactComponent {
         }
       );
     } else {
+      this.toastService.show('Please complete all required fields.', 'error');
       console.log('Form is invalid');
     }
   }
+
+
+
 }
